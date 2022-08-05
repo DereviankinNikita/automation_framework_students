34636e3b1739efefeb533189ae2a40c37fe24666
nigeriobike <n.eugenii2002@gmail.com>
2022-08-05 18:54:50 +0300

    constructors add

--- src/test/java/holders/Constructors.java
+++ src/test/java/holders/Constructors.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.interactions.Actions;
 import pageObjects.HomePageElements;
 import pageObjects.HomePageHelper;
+import pageObjects.RegPageElements;
+import pageObjects.RegPageHelper;
 
 public class Constructors {
     protected WebDriver driver;
@@ -19,6 +21,13 @@ public class Constructors {
     }
     protected HomePageHelper homePageObjHelper(){
         return new HomePageHelper(driver);
+    }
+    //reg
+    protected RegPageElements regPageElements(){
+        return new RegPageElements(driver);
+    }
+    protected RegPageHelper regPageHelper(){
+        return new RegPageHelper(driver);
     }
 
 }
