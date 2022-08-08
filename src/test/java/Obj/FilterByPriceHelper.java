a150c909211d9c30c386a290e0fc302483edea68
Maks Naumon <maks.zunets@gmail.com>
2022-08-08 16:15:45 +0300

    FilterHelpObj

--- src/test/java/Obj/FilterByPriceHelper.java
+++ src/test/java/Obj/FilterByPriceHelper.java
@@ -15,6 +15,43 @@ public class FilterByPriceHelper extends FilterByPriceObj{
         getButtonAccessoriesPriceRangeMax().click();
     }
 
+    public void clickCameraPhotoPriceMin(){
+        getButtonCameraPhotoPriceRangeMin().click();
+    }
+    public void clickCameraPhotoPriceMax(){
+        getButtonCameraPhotoPriceRangeMax().click();
+    }
+
+    public void clickJewelryPriceMin(){
+        getButtonJewelryPriceRangeMin().click();
+    }
+    public void clickJewelryPriceAverage(){
+        getButtonJewelryPriceRangeAverage().click();
+    }
+    public void clickJewelryPriceMax(){
+        getButtonJewelryPriceRangeMax().click();
+    }
+
+    public void clickBooksPriceMin(){
+        getButtonBooksPriceRangeMin().click();
+    }
+    public void clickBooksPriceAverage(){
+        getButtonBooksPriceRangeAverage().click();
+    }
+    public void clickBooksPriceMax(){
+        getButtonBooksPriceRangeMax().click();
+    }
+
+    public void clickDesktopsPriceMin(){
+        getButtonDesktopsPriceRangeMin().click();
+    }
+    public void clickDesktopsPriceAverage(){
+        getButtonDesktopsPriceRangeAverage().click();
+    }
+    public void clickDesktopsPriceMax(){
+        getButtonDesktopsPriceRangeMax().click();
+    }
+
 
 
     public void clickRemoveFilter(){
