17a51529b5bf67f256056ff06d2bf5ec1460a6cf
Maks Naumon <maks.zunets@gmail.com>
2022-08-12 17:28:01 +0300

    Test categories and Filter by prise(Fixed merge issues)

--- src/test/java/pageObjects/HomePageElements.java
+++ src/test/java/pageObjects/HomePageElements.java
@@ -13,9 +13,6 @@ public class HomePageElements extends DriverHolder {
     private String SubscribeButton = "//input[@id='newsletter-subscribe-button']";
     private String RegisterButton = "//*[@class='ico-register']";
     private String LogButton = "//*[@class='ico-login']";
-    private String giftCardsButton = "//*[@href='/gift-cards']";
-    private String computersButton = "//*[@href='/computers']";
-
 
 
     public WebElement getSubscribeBtn()
@@ -24,10 +21,4 @@ public class HomePageElements extends DriverHolder {
     }
     public  WebElement getRegisterBtn(){return getElByXpath(RegisterButton);}
     public  WebElement getLogBtn(){return getElByXpath(LogButton);}
-
-    public WebElement SubscribeBtn() {return getElByXpath(SubscribeButton); }
-    public  WebElement RegisterBtn(){return getElByXpath(RegisterButton);}
-    public  WebElement LogBtn(){return getElByXpath(RegisterButton);}
-    public WebElement getGiftCardsBtn(){return  getElsByXpath(giftCardsButton).get(0);}
-    public WebElement getComputersButton(){return  getElsByXpath(computersButton).get(0);}
 }
