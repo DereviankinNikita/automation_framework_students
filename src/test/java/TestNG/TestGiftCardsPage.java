4786af1634a50af6891cf1c7612a500399637bc3
CortUA <104569441+CortUA@users.noreply.github.com>
2022-08-12 13:54:54 +0300

    Assert Gift cards

--- src/test/java/TestNG/TestGiftCardsPage.java
+++ src/test/java/TestNG/TestGiftCardsPage.java
@@ -2,7 +2,11 @@ package TestNG;
 
 import Holder.TestInit;
 import Obj.HomePageObjHelper;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
 @Test
 public class TestGiftCardsPage extends TestInit {
 
@@ -10,6 +14,10 @@ public class TestGiftCardsPage extends TestInit {
         HomePageObjHelper homePageObjHelper = new HomePageObjHelper(driver);
         gotoHomePage();
         homePageObjHelper.clickGiftCards();
+        String virtualCards25 = driver.findElements(By.xpath("//*[@href='/25-virtual-gift-card']")).get(1).getText();
+        assertTrue(virtualCards25.contains("$25 Virtual Gift Card"));
+        String physicalCards50 = driver.findElements(By.xpath("//*[@href='/50-physical-gift-card']")).get(1).getText();
+        assertTrue(physicalCards50.contains("$50 Physical Gift Card"));
     }
 
 }
